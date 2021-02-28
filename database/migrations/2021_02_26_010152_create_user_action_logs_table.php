d60e65d3039d3b478640eaeea65d1af67e9d22fd
Gbeila Aliu Wahab <aliuwahab@gmail.com>
2021-02-28 14:15:41 +0100

    Add migrations

---
+++ database/migrations/2021_02_26_010152_create_user_action_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserActionLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_action_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('book_id')->constrained();
+            $table->enum('status', ['CHECKIN','CHECKOUT']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_action_logs');
+    }
+}
